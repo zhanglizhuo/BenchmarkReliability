b0e6b02d27516fe9001877fb161cf150c7e02c8e
Lizhuo Zhang <zhanglizhuo@gmail.com>
2026-07-01 10:32:42 +0800

    bump: v0.1.5 -> v0.1.6 (registry + CLI + SHA-256 + JOSS paper)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="benchmark-reliability",
-    version="0.1.5",
+    version="0.1.6",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     package_data={
